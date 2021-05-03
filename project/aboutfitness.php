c706f9fe5640e24211bfc2a45d3e1e6b61141aa4
vraj-ui <69664494+vraj-ui@users.noreply.github.com>
2021-05-03 22:07:00 +0530

    Add files via upload

---
+++ project/aboutfitness.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Fitness</title>
+	<!-- <link rel="stylesheet" type="text/css" href="historycss.css"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+</head>
+<style>
+    *{
+	margin: 0;
+	padding: 0;
+	font-family: 'Josefin Sans', sans-serif;
+    }
+
+    body{
+		background-image: linear-gradient(120deg,rgba(2, 80, 247, 0.89),rgba(255, 1, 1, 0.733));
+	    background-color: rgb(0, 0, 0);
+	    background-size: cover;
+	    background-position: center;
+    }
+
+    h1{
+        margin-left: 30px;
+        text-shadow: 3px 3px 4px rgb(255, 0, 0);
+    }
+
+    h2{ 
+        margin-left: 50px;
+        color: rgb(255, 238, 0);
+        text-shadow: 4px 4px 5px rgb(0, 225, 255);
+    }
+    h2 a{
+        text-decoration: none;
+    }
+    body .btnone{
+        padding: 5px;
+        border-radius: 1rem;
+        letter-spacing: 1px;
+        text-shadow: 1px 1px 2px rgb(255, 251, 0);
+	    background-color: rgb(104, 177, 245);
+    }
+    body a:hover{
+        background-color: #ff0000;
+        color: rgb(255, 236, 68);
+    }
+
+    ::-webkit-scrollbar
+    {
+        width: 10px;
+        background-color: #474747;
+    }
+
+    ::-webkit-scrollbar-thumb
+    {
+        background-color: rgb(0, 0, 0);
+        background-image: -webkit-linear-gradient(240deg,rgb(255, 0, 0) 0%,rgb(255, 1, 1) 25%, rgb(0, 17, 255) 75%, rgb(0, 17, 255) 100%);
+    }
+
+</style>
+<body>
+    <h1 class=" animate__animated animate__fadeInLeft "><p style="color: #fbff00"> ♦ Overview:</h1>
+        <h2 class="animate__animated animate__slow animate__slow animate__fadeIn">
+             <p><p>Apparatus such as barbells, jumping board, running path, tennis-balls, cricket field, and fencing area are used as exercises. In safe weather, outdoor locations are the most conducive to health. Gyms were popular in ancient Greece. Their curricula included self-defense, gymnastica medica, or physical therapy to help the sick and injured, and for physical fitness and sports, from boxing to dancing to skipping rope.</p><br> 
+                <p>Also had teachers of wisdom and philosophy. Community gymnastic events were done as part of the celebrations during various village festivals. In ancient Greece there was a phrase of contempt, "He can neither swim nor write." After a while, however, Olympic athletes began training in buildings specifically designed for them. Community sports never became as popular among ancient Romans as it had among the ancient Greeks. Gyms were used more as a preparation for military service or spectator sports. During the Roman Empire, the gymnastic art was forgotten. In the Dark Ages there were sword fighting tournaments and of chivalry; and after gunpowder was invented sword fighting began to be replaced by the sport of fencing, as well as schools of dagger fighting and wrestling and boxing.</p><br>                     
+                <p>the 18th century, Salzmann, German clergyman, opened a gym in Thuringia teaching bodily exercises, including running and swimming. Clias and Volker established gyms in London, and in 1825, Doctor Beck, a German immigrant, established the first gymnasium in the United States. It was found that gym pupils lose interest in doing the same exercises, partly because of age. Variety in exercises included skating, dancing, and swimming. Some gym activities can be done by 6 to 8-year-olds while age 16 has been considered mature enough for boxing and horseback riding.</p><br>          
+                <p>Ancient Greece, the gymnasion (γυμνάσιον) was a locality for both physical and intellectual education of young men. The latter meaning of intellectual education persisted in Greek, German and other languages to denote a certain type of school providing secondary education, the gymnasium, whereas in English the meaning of physical education pertained in the word 'gym'.</p><br>  <p>The Greek word gymnasium, which means "school for naked exercise," was used to designate a locality for the education of young men, including physical education (gymnastics, for example, exercise) which was customarily performed naked, as well as bathing, and studies. For the Greeks, physical education was considered as important as cognitive learning. Most Greek gymnasia had libraries that for use after relaxing in the baths.</p></p><br/>
+        </h2>
+
+    <h1 class=" animate__animated animate__fadeInLeft "><p style="color: #fbff00"> ♦ History:</h1>
+        <h2 class="animate__animated animate__slow animate__slow animate__fadeIn">
+                    <p ><p>The first recorded gymnasiums date back to over 3000 years ago in ancient Persia, where they were known as zurkhaneh, areas that encouraged physical fitness. The larger Roman Baths often had attached fitness facilities, the baths themselves sometimes being decorated with mosaics of local champions of sport. Gyms in Germany were an outgrowth of the Turnplatz,</p><br> <p>An outdoor space for gymnastics founded by German educator Friedrich Jahn in 1811 and later promoted by the Turners, a nineteenth-century political and gymnastic movement. The first American to open a public gym in the United States using Jahn's model was John Neal of Portland, Maine in 1827.</p><br> <p>First indoor gymnasium in Germany was probably the one built in Hesse in 1852 by Adolph Spiess.
+                        Through worldwide colonization, Great Britain expanded its national interest in sports and games to many countries. In the 1800s, programs were added to schools and college curricula that emphasized health, strength, and bodily measure. Sports drawn from European and British cultures thrived as college students and upper-class clubs financed competition. As a result, towns began building playgrounds that furthered interest in sports and physical activity.</p><br> <p>In the United States, the Turner movement thrived in the nineteenth and early twentieth centuries. The first Turners group was formed in London in 1848. The Turners built gymnasia in several cities like Cincinnati and St. Louis which had large German American populations. These gyms were utilized by adults and youth. For example, a young Lou Gehrig would frequent the Turner gym in New York City with his father.
+                        Interior of a gym in The Netherlands, around 1900 The Boston Young Men's Christian Union claims to be "America's First Gym". The YMCA first organized in Boston in 1851 and a smaller branch opened in Rangasville in 1852.</p><br> <p>Ten years later there were some two hundred YMCAs across the country, most of which provided gymnasia for exercise, games, and social interaction.
+                        <P>Today, gymnasia are commonplace in the United States. They are in virtually all U.S. colleges and high schools, as well as almost all middle schools and elementary schools. These facilities are used for physical education, intramural sports, and school gatherings. The number of gyms in the U.S. has more than doubled since the late 1980s.</p><br>
+        </h2>
+    <h2 class="animate__animated animate__slow animate__fadeIn"><a href="C:\xampp\htdocs\project\index.php" class="btnone">♦ Home</a></h2><br>
+</body>
+</html>
+
